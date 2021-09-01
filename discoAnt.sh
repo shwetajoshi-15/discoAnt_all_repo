501c019b4d319b8ba5cdac97fc6840c23bdb6b2d
shwetajoshi-15 <54519592+shwetajoshi-15@users.noreply.github.com>
2021-09-01 10:17:40 +1000

    Update discoAnt.sh

--- discoAnt.sh
+++ discoAnt.sh
@@ -24,6 +24,7 @@ echo "Making folders"
 mkdir -p $DISCOANT/$GENE
 mkdir -p $DISCOANT/$GENE/fasta_stats
 mkdir -p $DISCOANT/$GENE/minimap2
+mkdir -p $DISCOANT/$GENE/minimap2_target
 mkdir -p $DISCOANT/$GENE/transcriptclean
 mkdir -p $DISCOANT/$GENE/stringtie
 mkdir -p $DISCOANT/$GENE/sqanti3
@@ -31,21 +32,45 @@ mkdir -p $DISCOANT/$GENE/minimap2_metagene
 
 echo "Extracting the number of reads and read lengths"
 
+basename -s .fa $FASTA/*.fa | sed 's/^.*bar/bar/g' > $DISCOANT/$GENE/fasta_stats/barcodes.txt
+
+file=$DISCOANT/$GENE/fasta_stats/$GENE_stats.COMPLETED
+
+if [[ ! -f $DISCOANT/$GENE/fasta_stats/"$GENE"_stats.COMPLETED ]]
+
+then
+	
    for filename in $FASTA/*.fa
 
-   do
+	do
 
-   base=$(basename $filename .fa)
-   echo "On sample : $base"
-
-   cat $FASTA/${base}.fa | grep -c "^>" >> $DISCOANT/$GENE/fasta_stats/"$GENE"_total_number_of_reads.txt
-   mkdir -p $DISCOANT/$GENE/fasta_stats/"$GENE"_read_lengths
-   bioawk -c fastx '{print length($seq)}' $FASTA/${base}.fa > $DISCOANT/$GENE/fasta_stats/"$GENE"_read_lengths/${base}.txt
-   awk '{/>/&&++a||b+=length()}END{print b/a}' $FASTA/${base}.fa >> $DISCOANT/$GENE/fasta_stats/"$GENE"_mean_read_lengths.txt
+   	base=$(basename $filename .fa)
+   	echo "On sample : $base"
    
-   done
+   	cat $FASTA/${base}.fa | grep -c "^>" >> $DISCOANT/$GENE/fasta_stats/"$GENE"_tmp1.txt
+   	paste $DISCOANT/$GENE/fasta_stats/barcodes.txt $DISCOANT/$GENE/fasta_stats/"$GENE"_tmp1.txt | column -s $'\t' -t > $DISCOANT/$GENE/fasta_stats/"$GENE"_total_number_of_reads.txt
+
+   	mkdir -p $DISCOANT/$GENE/fasta_stats/"$GENE"_read_lengths
+   	bioawk -c fastx '{print length($seq)}' $FASTA/${base}.fa > $DISCOANT/$GENE/fasta_stats/"$GENE"_read_lengths/${base}.txt
+
+   	awk '{/>/&&++a||b+=length()}END{print b/a}' $FASTA/${base}.fa >> $DISCOANT/$GENE/fasta_stats/"$GENE"_tmp2.txt
+   	paste $DISCOANT/$GENE/fasta_stats/barcodes.txt $DISCOANT/$GENE/fasta_stats/"$GENE"_tmp2.txt | column -s $'\t' -t > $DISCOANT/$GENE/fasta_stats/"$GENE"_mean_read_lengths.txt
+
+ 	rm $DISCOANT/$GENE/fasta_stats/*tmp* && rm $DISCOANT/$GENE/fasta_stats/barcodes.txt && touch $DISCOANT/$GENE/fasta_stats/"$GENE"_stats.COMPLETED
+
+	done
+
+else 
+
+echo "FASTA stats are present in $DISCOANT/$GENE/fasta_stats"
+	
+fi
 
 echo "minimap2 - Mapping fasta files to genome"
+
+if [[ ! -f $DISCOANT/$GENE/minimap2/"$GENE"_sorted_pri_align.COMPLETED ]]
+
+then
 
    for filename in $FASTA/*.fa
 
@@ -59,15 +84,67 @@ echo "minimap2 - Mapping fasta files to genome"
    samtools view -h -F 2308 $DISCOANT/$GENE/minimap2/${base}_sorted.bam | samtools sort - > $DISCOANT/$GENE/minimap2/${base}_pri_sorted.bam 
    
    done
+   
+   touch $DISCOANT/$GENE/minimap2/"$GENE"_sorted_pri_align.COMPLETED
+
+else 
+
+echo "Alignments to the genome are present in $DISCOANT/$GENE/minimap2"
+	
+fi
+
+echo "Extracting alignments to the target region/gene"
+
+if [[ ! -f $DISCOANT/$GENE/minimap2_target/"$GENE"_sorted_pri_tar_align.COMPLETED  ]]
+
+then
+
+   for filename in $FASTA/*.fa
+
+   do
+
+   base=$(basename $filename .fa)
+   echo "On sample : $base"
+   
+   samtools index $DISCOANT/$GENE/minimap2/${base}_pri_sorted.bam
+   samtools view $DISCOANT/$GENE/minimap2/${base}_pri_sorted.bam "$CHR:$GENE_START-$GENE_END" > $DISCOANT/$GENE/minimap2_target/${base}_pri_tar_sorted.bam
+ 
+   done
+
+   touch $DISCOANT/$GENE/minimap2_target/"$GENE"_sorted_pri_tar_align.COMPLETED
+
+else 
+
+echo "Filtered primary alignments are present in $DISCOANT/$GENE/minimap2_target"
+	
+fi
+
 
 echo "Merging minimap2 primary alignments"
 
-   samtools merge $DISCOANT/$GENE/minimap2/"$GENE"_merged.bam $DISCOANT/$GENE/minimap2/*_pri_sorted.bam 
-   samtools sort $DISCOANT/$GENE/minimap2/"$GENE"_merged.bam > $DISCOANT/$GENE/minimap2/"$GENE"_merged_sorted.bam
-   samtools index $DISCOANT/$GENE/minimap2/"$GENE"_merged_sorted.bam 
-   samtools view -h -o $DISCOANT/$GENE/minimap2/"$GENE"_merged.sam $DISCOANT/$GENE/minimap2/"$GENE"_merged_sorted.bam
+if [[ ! -f $DISCOANT/$GENE/minimap2_target/"$GENE"_sorted_pri_tar_merged_align.COMPLETED ]]
 
-echo "Correecting the merged primary alignments with TranscriptClean"
+then
+
+   samtools merge $DISCOANT/$GENE/minimap2_target/"$GENE"_merged.bam $DISCOANT/$GENE/minimap2_target/*_pri_tar_sorted.bam 
+   samtools sort $DISCOANT/$GENE/minimap2_target/"$GENE"_merged.bam > $DISCOANT/$GENE/minimap2_target/"$GENE"_merged_sorted.bam
+   samtools index $DISCOANT/$GENE/minimap2_target/"$GENE"_merged_sorted.bam 
+   samtools view -h -o $DISCOANT/$GENE/minimap2_target/"$GENE"_merged.sam $DISCOANT/$GENE/minimap2_target/"$GENE"_merged_sorted.bam
+
+   touch $DISCOANT/$GENE/minimap2_target/"$GENE"_sorted_pri_tar_merged_align.COMPLETED
+   
+else 
+
+echo "Merged alignments are present in $DISCOANT/$GENE/minimap2_target"
+	
+fi
+
+
+echo "Correcting the merged primary alignments with TranscriptClean"
+
+if [[ ! -f $DISCOANT/$GENE/transcriptclean/"$GENE"_merged_clean_sorted.COMPLETED ]]
+
+then
 
    python $PROGRAMS/TranscriptClean/TranscriptClean.py \
    --sam $DISCOANT/$GENE/minimap2/"$GENE"_merged.sam \
@@ -76,13 +153,52 @@ echo "Correecting the merged primary alignments with TranscriptClean"
 
    samtools view -S -h -b $DISCOANT/$GENE/transcriptclean/"$GENE"_clean.sam | samtools sort - > $DISCOANT/$GENE/transcriptclean/"$GENE"_clean_sorted.bam
 
+   touch $DISCOANT/$GENE/transcriptclean/"$GENE"_merged_clean_sorted.COMPLETED
+
+else
+
+echo "TranscriptClean-ed alignments are present in $DISCOANT/$GENE/transcriptclean"
+	
+fi
+
+
 echo "Constructing transcripts based on the corrected alignments"
 
-   stringtie -t $DISCOANT/$GENE/transcriptclean/"$GENE"_clean_sorted.bam \
+if [[ ! -f $DISCOANT/$GENE/stringtie/"$GENE"_transcripts_pre_clean.COMPLETED ]]
+
+then
+
+   stringtie -t $DISCOANT/$GENE/minimap2_target/"$GENE"_merged_sorted.bam \
    -G $REF_HG38/gencode.v35.annotation.gtf \
    -A $DISCOANT/$GENE/stringtie/"$GENE"_gene_abund.tab \
    -C $DISCOANT/$GENE/stringtie/"$GENE"_cov_ref.gtf \
    -o $DISCOANT/$GENE/stringtie/"$GENE"_STRINGTIE.gtf
+   
+   touch $DISCOANT/$GENE/stringtie/"$GENE"_transcripts_pre_clean.COMPLETED
+
+else
+
+echo "Transcripts constructed pre correction are present in $DISCOANT/$GENE/stringtie"
+	
+fi
+
+if [[ ! -f $DISCOANT/$GENE/stringtie/"$GENE"_transcripts_post_clean.COMPLETED ]]
+
+then
+
+   stringtie -t $DISCOANT/$GENE/transcriptclean/"$GENE"_clean_sorted.bam \
+   -G $REF_HG38/gencode.v35.annotation.gtf \
+   -A $DISCOANT/$GENE/stringtie/"$GENE"_clean_gene_abund.tab \
+   -C $DISCOANT/$GENE/stringtie/"$GENE"_clean_cov_ref.gtf \
+   -o $DISCOANT/$GENE/stringtie/"$GENE"_clean_STRINGTIE.gtf
+   
+   touch $DISCOANT/$GENE/stringtie/"$GENE"_transcripts_post_clean.COMPLETED
+
+else
+
+echo "Transcripts constructed post correction are present in $DISCOANT/$GENE/stringtie"
+	
+fi
 
 echo "Annotating the transcripts with SQANTI3"
 
