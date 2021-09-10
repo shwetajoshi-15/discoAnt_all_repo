c707e4955fdf595b62fbb0c7c2961e6172b21ca0
shwetajoshi-15 <54519592+shwetajoshi-15@users.noreply.github.com>
2021-09-10 15:58:31 +1000

    Update discoAnt.sh

--- discoAnt.sh
+++ discoAnt.sh
@@ -1,19 +1,22 @@
 #/bin/bash!
 
 ## This script performs the following steps
-## 1. Quantify control for the fasta files 
+## 1. Quality control for the fasta files 
 ## 1.a. Generates a file with no. of reads
 ## 1.b. average read length
 ## 1.c. read length in each barcode/sample 
 ## 2. Align the sample fasta files to reference genome
-## 3. Merge the primary alignments
-## 4. Generate transcript files (GTF) for the merged alignments
-## 4. Classify the transcripts based on known annotations 
-## 6. Create a metagene based on the transcripts
-## 7. Align the samples fasta files to the metagene
-## 8. Create transcript counts based on the alignments to the metagene
-## 9. Normalise the counts
-## 10. Generate a heatmap and PCA plot
+## 3. Filter out off-target alignments
+## 4. Merge alignments from all samples
+## 5. Correct the alignments based on reference genome
+## 6. Generate transcript files (GTF) for the merged alignments
+## 7. Classify the transcripts based on known annotations 
+## 8.a. Create a metagene based on known and novel exons
+## 8.b. Create a GTF file for the metagene
+## 9. Align the samples fasta files to the metagene
+## 10. Generate counts based on the alignments to the metagene
+## 11. Merge the transcript annotations and counts in "results"
+## 12. Generate a heatmap and PCA plot
 
 
 source discoAnt_params.txt
@@ -30,6 +33,11 @@ mkdir -p $DISCOANT/$GENE/stringtie
 mkdir -p $DISCOANT/$GENE/sqanti3
 mkdir -p $DISCOANT/$GENE/minimap2_metagene
 mkdir -p $DISCOANT/$GENE/featurecounts
+mkdir -p $DISCOANT/$GENE/results
+
+##########                                          ##########
+##########  1. Quality control for the fasta files  ##########
+##########                                          ##########
 
 echo "Extracting the number of reads and read lengths"
 
@@ -61,6 +69,12 @@ else
 echo "FASTA stats are present in $DISCOANT/$GENE/fasta_stats"	
 fi
 
+
+##########                                                      ##########
+########## 2. Align the sample fasta files to reference genome  ##########
+##########                                                      ##########
+
+
 echo "minimap2 - Mapping fasta files to genome"
 
 if [[ ! -f $DISCOANT/$GENE/minimap2/"$GENE"_sorted_pri_align.COMPLETED ]]
@@ -81,6 +95,10 @@ else
 echo "Alignments to the genome are present in $DISCOANT/$GENE/minimap2"	
 fi
 
+##########                                      ##########
+########## 3. Filter out off-target alignments  ##########
+##########                                      ##########
+
 echo "Extracting alignments to the target region/gene"
 
 if [[ ! -f $DISCOANT/$GENE/minimap2_target/"$GENE"_sorted_pri_tar_align.COMPLETED  ]]
@@ -99,6 +117,9 @@ else
 echo "Filtered primary alignments are present in $DISCOANT/$GENE/minimap2_target"	
 fi
 
+##########                                      ##########
+########## 4. Merge alignments from all samples ##########
+##########                                      ##########
 
 echo "Merging minimap2 primary alignments"
 
@@ -116,6 +137,9 @@ else
 echo "Merged alignments are present in $DISCOANT/$GENE/minimap2_target"	
 fi
 
+##########                                                     ##########
+########## 5. Correct the alignments based on reference genome ##########
+##########                                                     ##########
 
 echo "Correcting the merged primary alignments with TranscriptClean"
 
@@ -135,6 +159,9 @@ else
 echo "TranscriptClean-ed alignments are present in $DISCOANT/$GENE/transcriptclean"	
 fi
 
+##########                                                              ##########
+########## 6. Generate transcript files (GTF) for the merged alignments ##########
+##########                                                              ##########
 
 echo "Constructing transcripts based on the corrected alignments"
 
@@ -168,6 +195,10 @@ else
 echo "Transcripts constructed post correction are present in $DISCOANT/$GENE/stringtie"
 fi
 
+##########                                                        ##########
+########## 7. Classify the transcripts based on known annotations ##########
+##########                                                        ##########
+
 echo "Annotating the transcripts with SQANTI3"
 
 if [[ ! -f $DISCOANT/$GENE/stringtie/"$GENE"_sqanti3_clean.COMPLETED ]]
@@ -188,7 +219,12 @@ else
 echo "Annotated transcripts are present in $DISCOANT/$GENE/sqanti3"
 fi   
 
+##########                                                       ##########
+########## 8.a. Create a metagene based on known and novel exons ##########
+##########                                                       ##########
+
 echo "Creating a metagene FASTA"
+
 ## check if the strand variable is assigned correctly
 if [[ ! -f $DISCOANT/$GENE/stringtie/"$GENE"_metagene.COMPLETED ]]
 then
@@ -207,11 +243,28 @@ else
 echo "Metagene is present in $DISCOANT/$GENE/stringtie"	
 fi   
 
-echo "Modifying stringtie GTF to create a metagene GTF"
-echo "(under construction; create this file on text editor/excel)"
+##########                                         ##########
+########## 8.b. Create a GTF file for the metagene ##########
+##########                                         ##########
 
-	##cat $DISCOANT/$GENE/stringtie/"$GENE"_clean_STRINGTIE.gtf | awk '{ if ($3 == "exon") { print } }' | sed 's/; exon_number.*/;/' > $DISCOANT/$GENE/stringtie/GRIA1_STRINGTIE_modified_tmp1.gtf 
+echo "Modifying stringtie GTF to create a metagene GTF"
+	
+	Rscript $PROGRAMS/exon_coord_conversion_plus_strand.R --col1 $DISCOANT/$GENE/stringtie/"$GENE"_exon_start.txt \
+	--col2 $DISCOANT/$GENE/stringtie/"$GENE"_exon_end.txt \
+	--out $DISCOANT/$GENE/stringtie/"$GENE"_metagene_exon_coord.txt
+	
+	cat $DISCOANT/$GENE/stringtie/"$GENE"_clean_STRINGTIE.gtf | \
+	awk '{ if ($3 == "exon") { print $9,$10,$11,$12 } }' > $DISCOANT/$GENE/stringtie/"$GENE"_STRINGTIE_modified_tmp1.gtf 
+	cat $DISCOANT/$GENE/stringtie/"$GENE"_metagene_exon_coord.txt | \
+	awk '{print -v "meta_gene_"$GENE"""\t""Stringtie""\t""exon""\t"$1"\t"$2"\t"".""\t""$STRAND""\t""."}' > $DISCOANT/$GENE/stringtie/"$GENE"_STRINGTIE_modified_tmp2.gtf
  
+ 	paste $DISCOANT/$GENE/stringtie/"$GENE"_STRINGTIE_modified_tmp2.gtf \
+	$DISCOANT/$GENE/stringtie/"$GENE"_STRINGTIE_modified_tmp1.gtf > $DISCOANT/$GENE/stringtie/"$GENE"_STRINGTIE_modified.gtf
+	
+##########                                                  ##########
+########## 9. Align the samples fasta files to the metagene ##########
+##########                                                  ##########
+
 echo "Mapping sample FASTA to the metagene"
 
 if [[ ! -f $DISCOANT/$GENE/minimap2_metagene/"$GENE"_sorted_pri_align.COMPLETED ]]
@@ -231,6 +284,10 @@ else
 echo "Alignments to the metagene are present in $DISCOANT/$GENE/minimap2_metagene"	
 fi
 
+##########                                                             ##########
+########## 10. Generate counts based on the alignments to the metagene ##########
+##########                                                             ##########
+
 echo "Generating read counts for the metagene alignments"
 
 if [[ ! -f $DISCOANT/$GENE/featurecounts/"$GENE"_metagene_counts.COMPLETED ]]
@@ -239,14 +296,27 @@ then
 	-o $DISCOANT/$GENE/featurecounts/"$GENE"_counts.txt $DISCOANT/$GENE/minimap2_metagene/*_pri_sorted.bam
 
 	cat $DISCOANT/$GENE/featurecounts/"$GENE"_counts.txt | cut -f2,3,4,5,6 --complement | awk 'FNR > 2' > $DISCOANT/$GENE/featurecounts/"$GENE"_counts_matrix.txt
-	cat $DISCOANT/$GENE/"$GENE"_samplenames.txt | tr '\n' '\t' | paste - $DISCOANT/$GENE/featurecounts/"$GENE"_counts_matrix.txt > $DISCOANT/$GENE/featurecounts/"$GENE"_counts_matrix_samplenames.txt
+	cat $DISCOANT/$GENE/"$GENE"_samplenames.txt | tr '\n' '\t' | paste - $DISCOANT/$GENE/featurecounts/"$GENE"_counts_matrix.txt | column -s $'\t' -t > $DISCOANT/$GENE/featurecounts/"$GENE"_counts_matrix_samplenames.txt
 
 	touch $DISCOANT/$GENE/featurecounts/"$GENE"_metagene_counts.COMPLETED
 else
 echo "Counts matrix is present in $DISCOANT/$GENE/featurecounts"	
 fi
 
+##########                                                              ##########
+########## 11. Merge the transcript annotations and counts in "results" ##########
+##########                                                              ##########
+
 echo "Merging SQANTI3 annotations with counts matrix"
+
+paste $DISCOANT/$GENE/sqanti3/"$GENE"_sqanti_matrix.txt $DISCOANT/$GENE/featurecounts/"$GENE"_counts_matrix_samplenames.txt | column -s $'\t' -t > $DISCOANT/$GENE/results/$GENE_annotated_transcript_counts.txt
+
+##########                                     ##########
+########## 12. Generate a heatmap and PCA plot ##########
+##########                                     ##########
+
+
+## under construction 
 
 
 
